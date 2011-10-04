465aa9c52157761ee687608333eec66e77582b98
albair <albaiglesiasruiz@gmail.com>
2011-10-04 11:33:25 +0200

    Add validation to ensure name are specified when creating projects

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -3,23 +3,49 @@ Given /^(?:|I )am on (.+)$/ do |page_name|
 end
 
 When /^I follow "([^"]*)"$/ do |arg1|
-  visit new_project_path#@project = Project.new#pending # express the regexp above with the code you wish you had
+  click_link arg1
 end
 
 When /^I fill in "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
-  #pending # express the regexp above with the code you wish you had
-end
-
-When /^I should be on the project page for "([^"]*)"$/ do |arg1|
-  #pending # express the regexp above with the code you wish you had
+ fill_in arg1, :with=>arg2 # express the regexp above with the code you wish you had
 end
 
 
 When /^I press "([^"]*)"$/ do |arg1|
-  #pending # express the regexp above with the code you wish you had
+  click_button arg1
 end
 
-Then /^I should see "([^"]*)"$/ do |arg1|
-  #pending # express the regexp above with the code you wish you had
+
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
 end
 
+Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath('//*', :text => regexp)
+  else
+    assert page.has_xpath?('//*', :text => regexp)
+  end
+end
+
+Then /^(?:|I )should not see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_no_content(text)
+  else
+    assert page.has_no_content?(text)
+  end
+end
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
