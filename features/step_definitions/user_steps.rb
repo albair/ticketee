27f1b8d615c88e7dd25bc29fd4701d0e47c2a2f6
albair <albaiglesiasruiz@gmail.com>
2011-10-07 10:39:30 +0200

    Users can now upload 3 files at a time

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -23,7 +23,7 @@ Given /^I am signed in as "([^\"]*)"$/ do |email|
   steps("Given I am signed in as them")
 end
 
-  When /^I check "([^"]*)"$/ do |field|
+When /^I check "([^"]*)"$/ do |field|
   check field
 end
 
