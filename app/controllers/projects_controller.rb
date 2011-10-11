e0536a64e951bd7e306e20c79e8cf8d288abbaab
albair <albaiglesiasruiz@gmail.com>
2011-10-11 17:07:03 +0200

    Add email ticket notifications

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,67 +3,72 @@
 class ProjectsController < ApplicationController
 
 
-   before_filter :authorize_admin!, :except => [:index, :show]
-   before_filter :authenticate_user!, :only => [:index,:show]
-   before_filter :find_project, :only => [:show,:edit,:update,:destroy]
+    before_filter :authorize_admin!, :except => [:index, :show]
+    before_filter :authenticate_user!, :only => [:index,:show]
+    before_filter :find_project, :only => [:show,:edit,:update,:destroy]
 
-   def index
-     @projects = Project.for(current_user).all
-   end
+    def index
+       @projects = Project.for(current_user).all
+    end
 
-  def new
-    @project = Project.new
-  end
 
-  def edit
-   #@project = Project.find(params[:id])
-  end
 
-  def create
-     @project = Project.new(params[:project])
-     if @project.save
-       flash[:notice] = "Project has been created."
+
+
+    def new
+      @project = Project.new
+    end
+
+    def edit
+     #@project = Project.find(params[:id])
+    end
+
+    def create
+       @project = Project.new(params[:project])
+       if @project.save
+         flash[:notice] = "Project has been created."
+         redirect_to @project
+       else
+         flash[:alert] = "Project has not been created."
+         render :action => "new"
+       end
+    end
+
+     def show
+      @tickets = @project.tickets
+    end
+
+    def update
+     #@project = Project.find(params[:id])
+     if @project.update_attributes(params[:project])
+       flash[:notice] = "Project has been updated."
        redirect_to @project
      else
-       flash[:alert] = "Project has not been created."
-       render :action => "new"
+       flash[:alert] = "Project has not been updated."
+       render :action => "edit"
      end
-  end
+    end
 
+    def destroy
+      #@project = Project.find(params[:id])
+      @project.destroy
+      flash[:notice] = "Project has been deleted."
 
-  def show
-    @tickets = @project.tickets
-  end
+      redirect_to projects_path
+    end
 
+    def show
+      @tickets = @project.tickets
+    end
 
-
-  def update
-   #@project = Project.find(params[:id])
-   if @project.update_attributes(params[:project])
-     flash[:notice] = "Project has been updated."
-     redirect_to @project
-   else
-     flash[:alert] = "Project has not been updated."
-     render :action => "edit"
-   end
-  end
-
-  def destroy
-    #@project = Project.find(params[:id])
-    @project.destroy
-    flash[:notice] = "Project has been deleted."
-
-    redirect_to projects_path
-  end
-
-   private
-   def find_project
-     @project = Project.for(current_user).find(params[:id])
-     rescue ActiveRecord::RecordNotFound
-     flash[:alert] = "The project you were looking" +
-     " for could not be found."
-     redirect_to projects_path
-   end
+     private
+     def find_project
+       @project = Project.for(current_user).find(params[:id])
+       rescue ActiveRecord::RecordNotFound
+       flash[:alert] = "The project you were looking" +
+       " for could not be found."
+       redirect_to projects_path
+     end
 
 
 
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,12 +1,17 @@
 class Ticket < ActiveRecord::Base
-  validates :title, :presence => true
-  validates :description, :presence => true
-  validates :description, :presence => true, :length => { :minimum => 10 }
+
 
   searcher do
     label :tag, :from => :tags, :field => :name
     label :state, :from => :state, :field => "name"
   end
+
+
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :description, :presence => true, :length => { :minimum => 10 }
+
+
 
   belongs_to :project
   belongs_to :state
@@ -16,6 +21,10 @@ class Ticket < ActiveRecord::Base
   has_many :comments
   has_and_belongs_to_many :tags
 
+  has_and_belongs_to_many :watchers, :join_table => "ticket_watchers",:class_name => "User"
+
+  after_create :creator_watches_me
+
   def tag!(tags)
     tags = tags.split(" ").map do |tag|
       Tag.find_or_create_by_name(tag)
@@ -24,6 +33,10 @@ class Ticket < ActiveRecord::Base
     self.tags << tags
   end
 
+  private
+  def creator_watches_me
+   self.watchers << user
+  end
 
 
 
