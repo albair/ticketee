38ebfbf813777592a23c26b4eca00fdd8f11395f
albair <albaiglesiasruiz@gmail.com>
2011-10-11 12:15:08 +0200

    Users may now search for tickets by state or tag

--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -1,11 +1,15 @@
-Given /^"([^"]*)" has created a ticket for this project:$/ do |email, table|
+Given /^"([^\"]*)" has created a ticket for this project:$/ do |email, table|
   table.hashes.each do |attributes|
     tags = attributes.delete("tags")
-    attributes.merge!(:user => User.find_by_email!(email))
-    ticket = @project.tickets.create!(attributes)
+    state = attributes.delete("state")
+    ticket = @project.tickets.create!(
+                    attributes.merge!(:user =>User.find_by_email!(email)))
+    ticket.state = State.find_or_create_by_name(state) if state
     ticket.tag!(tags) if tags
+    ticket.save
   end
 end
+
 
 When /^I attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
   attach_file(field, File.expand_path(path))
